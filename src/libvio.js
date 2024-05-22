f847ec63d42708e5d00d886447ddd338d6c77d9c
songbirdzz <165868972+songbirdzz@users.noreply.github.com>
2024-05-22 15:42:00 +0000

    Update libvio adapter

--- src/libvio.js
+++ src/libvio.js
@@ -25,7 +25,7 @@ export async function getSources(seriesId) {
     const resp = await fetch(`https://www.libvio.pw${seriesId}`);
     const doc = parse(await resp.text());
     const ul = doc.querySelector('.stui-content__playlist');
-    const episodes = ul.childNodes.map(li => {
+    const episodes = ul.querySelectorAll('li').map(li => {
         const a = li.querySelector('a');
         return {
             episodeId: a.attributes.href,
